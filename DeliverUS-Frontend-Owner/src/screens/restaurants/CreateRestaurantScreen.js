72380540e458d495f00bd350dc6461d4239bca33
pablo-lpezz <pablophue@alum.us.es>
2026-04-08 11:19:40 +0200

    -changes

--- DeliverUS-Frontend-Owner/src/screens/restaurants/CreateRestaurantScreen.js
+++ DeliverUS-Frontend-Owner/src/screens/restaurants/CreateRestaurantScreen.js
@@ -15,7 +15,75 @@ import TextError from '../../components/TextError'
 import ImagePicker from '../../components/ImagePicker'
 
 export default function CreateRestaurantScreen({ navigation }) {
-
-
-
+  return (
+    <View style={{ alignItems: 'center' }}>
+      <View style={{ width: '60%' }}>
+      <InputItem
+        name='sampleInput'
+        label='Sample input'
+      />
+      <InputItem
+        name='sampleInput'
+        label='Sample input'
+      />
+      <InputItem
+        name='sampleInput'
+        label='Sample input'
+      />
+      <InputItem
+        name='sampleInput'
+        label='Sample input'
+      />
+      <InputItem
+        name='sampleInput'
+        label='Sample input'
+      />
+      <InputItem
+        name='sampleInput'
+        label='Sample input'
+      />
+      <InputItem
+        name='sampleInput'
+        label='Sample input'
+      />
+      <InputItem
+        name='sampleInput'
+        label='Sample input'
+      />
+      <Pressable
+        onPress={() => console.log('Button pressed')
+        }
+        style={({ pressed }) => [
+          {
+            backgroundColor: pressed
+              ? GlobalStyles.brandPrimaryTap
+              : GlobalStyles.brandPrimary
+          },
+          styles.button
+        ]}>
+        <TextRegular textStyle={styles.text}>
+          Create restaurant
+        </TextRegular>
+      </Pressable>
+      </View>
+    </View>
+  )
 }
+
+const styles = StyleSheet.create({
+button: {
+    borderRadius: 8,
+    height: 40,
+    padding: 10,
+    width: '100%',
+    marginTop: 20,
+    marginBottom: 20
+  },
+  text: {
+    fontSize: 16,
+    color: 'white',
+    textAlign: 'center',
+    marginLeft: 5
+  }
+})
+
